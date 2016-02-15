9f5f28c3fb09b6f19afee7588d8a1fe1ed934633
Xinxing Jiang <jiangxinxing@kingdowin.com>
2016-02-15 15:21:48 +0800

    feat: first commit

---
+++ CalPerDay/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CalPerDay
+//
+//  Created by Xinxing Jiang on 2/15/16.
+//  Copyright © 2016 jiang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
